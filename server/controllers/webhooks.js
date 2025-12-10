faccbcb4fe394d9627db0d18582895fa4e1722d2
ShrishtiKhurani <shrishtikhurani246@gmail.com>
2025-12-10 15:53:53 +0530

    add console.log

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -4,6 +4,9 @@ import User from "../models/User.js";
 // Api controller function to manage clerk user with database
 
 export const clerkWebhooks = async (req, res) => {
+      const { data, type } = req.body;
+    console.log("WEBHOOK HIT ====>", req.body);
+
   try {
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
@@ -13,7 +16,7 @@ export const clerkWebhooks = async (req, res) => {
       "svix-timestamp": req.headers["svix-timestamp"],
     }); 
 
-    const { data, type } = req.body;
+
 
     switch (type) {
       case "user.created": {
