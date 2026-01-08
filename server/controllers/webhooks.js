505ec2c6790c50f270d159dde5a02ad2a49c13f3
ShrishtiKhurani <shrishtikhurani246@gmail.com>
2026-01-08 11:15:37 +0530

    webhook updated

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,7 +1,59 @@
-import Stripe from "stripe";
+import { Webhook } from "svix";
 import User from "../models/User.js";
 import { Purchase } from "../models/Purchase.js";
 import Course from "../models/Course.js";
+import Stripe from "stripe";
+
+export const clerkWebhooks = async (req, res) => {
+  try {
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    await whook.verify(JSON.stringify(req.body), {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    });
+
+    const { data, type } = req.body;
+
+    switch (type) {
+      case "user.created": {
+        const userData = {
+          _id: data.id,
+          email: data.email_addresses[0].email_address,
+          name: `${data.first_name} ${data.last_name}`,
+          imageUrl: data.image_url,
+        };
+        await User.create(userData);
+        res.json({});
+        break;
+      }
+
+      case "user.updated": {
+        const userData = {
+          email: data.email_addresses[0].email_address,
+          name: `${data.first_name} ${data.last_name}`,
+          imageUrl: data.image_url,
+        };
+        await User.findByIdAndUpdate(data.id, userData);
+        res.json({});
+        break;
+      }
+
+      case "user.deleted": {
+        await User.findByIdAndDelete(data.id);
+        res.json({});
+        break;
+      }
+
+      default:
+        break;
+    }
+  } catch (err) {
+    console.log("WEBHOOK ERROR =>", err.message);
+    res.status(400).json({ error: err.message });
+  }
+};
 
 const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
 
@@ -65,9 +117,11 @@ export const stripeWebhooks = async (req, res) => {
         break;
       }
 
-      // Checkout Session Complete
+      // Checkout Session Complete 
       case "checkout.session.completed": {
         const session = event.data.object;
+
+        // Find purchase by Checkout Session ID or Payment Intent ID
         const purchaseData = await Purchase.findOne({
           $or: [
             { checkoutSessionId: session.id },
